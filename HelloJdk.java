32bfa42416cae283d2a080b2e617309d4ebf5fb0
Rolik8797 <117454607+Rolik8797@users.noreply.github.com>
2022-11-04 21:55:39 +0300

    Update HelloJdk.java

--- HelloJdk.java
+++ HelloJdk.java
@@ -1,6 +1,6 @@
 public class HelloJdk {
 
-    public static void main(String[] args) {
+    public static void master(String[] args) {
 
             System.out.println("Ура! Этот код работает!");
     }
